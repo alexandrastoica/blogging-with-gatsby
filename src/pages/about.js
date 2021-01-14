efc250e7a2eaded79c1f427a6b876acd09721bb5
Alexandra Stoica <ralexandrastoica@gmail.com>
2021-01-14 12:15:16 +0200

    Wrap the pages within the layout

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import Layout from '../components/layout';
 
 export default () => (
-  <>
+  <Layout>
     <h1>About me</h1>
     <p>This is my blog</p>
-    <Link to="/">&larr; back to home</Link>
-  </>
+    <Link to="/">&larr; Back to home</Link>
+  </Layout>
 );
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import Layout from '../components/layout';
 
 export default () => (
-  <>
+  <Layout>
     <h1>Home</h1>
     <p>Welcome</p>
-    <Link to="/about">learn about me &rarr;</Link>
-  </>
+    <Link to="/about">Learn about me &rarr;</Link>
+  </Layout>
 );
