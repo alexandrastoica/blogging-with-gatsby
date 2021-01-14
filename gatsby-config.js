4cd9bd3ecaa79e07d2328ae0d4fdb60e93523800
Alexandra Stoica <ralexandrastoica@gmail.com>
2021-01-14 13:00:58 +0200

    Add site metadata with GraphQL

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  plugins: ['gatsby-plugin-emotion'],
+  siteMetadata: {
+    title: 'Blogging with Gatsby',
+    description: 'A blog app using React, Gatsby, and GraphQL.',
+  },
+  plugins: ['gatsby-plugin-emotion', 'gatsby-plugin-react-helmet'],
 };
