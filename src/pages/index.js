62a294641bbe7a61f90d253733bb47bc9506acdd
Alexandra Stoica <ralexandrastoica@gmail.com>
2021-01-15 11:13:38 +0200

    Clean up redundant code

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'gatsby';
 import Layout from '../components/layout';
 import PostPreview from '../components/post-preview';
 import usePosts from '../hooks/use-posts';
@@ -12,10 +11,6 @@ export default () => {
     <>
       <Hero />
       <Layout>
-        {/* <h1>Home</h1>
-        <p>Welcome</p>
-        <Link to="/about">Learn about me &rarr;</Link> */}
-
         <h2>Read my blog</h2>
         {posts.map(post => (
           <PostPreview key={post.slug} post={post}></PostPreview>
