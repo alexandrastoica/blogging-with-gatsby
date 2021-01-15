54cfdb9b2c9d7a7aa82eb4024703b5d153f883bb
Alexandra Stoica <ralexandrastoica@gmail.com>
2021-01-15 10:23:13 +0200

    Create a hero component

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,20 +3,24 @@ import { Link } from 'gatsby';
 import Layout from '../components/layout';
 import PostPreview from '../components/post-preview';
 import usePosts from '../hooks/use-posts';
+import Hero from '../components/hero';
 
 export default () => {
   const posts = usePosts();
 
   return (
-    <Layout>
-      <h1>Home</h1>
-      <p>Welcome</p>
-      <Link to="/about">Learn about me &rarr;</Link>
+    <>
+      <Hero />
+      <Layout>
+        {/* <h1>Home</h1>
+        <p>Welcome</p>
+        <Link to="/about">Learn about me &rarr;</Link> */}
 
-      <h2>Read my blog</h2>
-      {posts.map(post => (
-        <PostPreview key={post.slug} post={post}></PostPreview>
-      ))}
-    </Layout>
+        <h2>Read my blog</h2>
+        {posts.map(post => (
+          <PostPreview key={post.slug} post={post}></PostPreview>
+        ))}
+      </Layout>
+    </>
   );
 };
